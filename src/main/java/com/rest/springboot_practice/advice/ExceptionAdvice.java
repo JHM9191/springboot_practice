16bcc80f080e504c2f6eb468b67587e38f9badf3
JHM9191 <hm.jo.9191@gmail.com>
2020-11-03 17:40:21 +0900

    handling exception using messageSource

--- src/main/java/com/rest/springboot_practice/advice/ExceptionAdvice.java
+++ src/main/java/com/rest/springboot_practice/advice/ExceptionAdvice.java
@@ -4,6 +4,8 @@ import com.rest.springboot_practice.advice.exception.CUserNotFoundException;
 import com.rest.springboot_practice.model.response.CommonResult;
 import com.rest.springboot_practice.service.ResponseService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -16,19 +18,30 @@ import javax.servlet.http.HttpServletRequest;
 public class ExceptionAdvice {
     private final ResponseService responseService;
 
-//    @ExceptionHandler(Exception.class)
-//
-//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-//
-//    protected CommonResult defaultException(HttpServletRequest request, Exception e) {
-//        return responseService.getFailResult();
-//    }
+    private final MessageSource messageSource;
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    protected CommonResult defaultException(HttpServletRequest request, Exception e) {
+        // 예외 처리의 메세지를 MessageSource에서 가져오도록 수정해준다.
+        return responseService.getFailResult(Integer.valueOf(getMessage("unKnown.code")), getMessage("unKnown.msg"));
+    }
 
     @ExceptionHandler(CUserNotFoundException.class)
-
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-
     protected CommonResult userNotFoundException(HttpServletRequest request, CUserNotFoundException e) {
-        return responseService.getFailResult();
+        System.out.println(getMessage("unKnown.code"));
+        // 예외 처리의 메세지를 MessageSource에서 가져오도록 수정해준다.
+        return responseService.getFailResult(Integer.valueOf(getMessage("userNotFound.code")), getMessage("userNotFound.msg"));
+    }
+
+    // code 정보에 해당하는 메세지를 조회한다.
+    private String getMessage(String code) {
+        return getMessage(code, null);
+    }
+
+    // code 정보, 추가 argument로 현재 locale에 맞는 메세지를 조회한다.
+    private String getMessage(String code, Object[] args) {
+        return messageSource.getMessage(code, args, LocaleContextHolder.getLocale());
     }
 }
--- src/main/java/com/rest/springboot_practice/controller/v1/UserController.java
+++ src/main/java/com/rest/springboot_practice/controller/v1/UserController.java
@@ -30,7 +30,9 @@ public class UserController {
 
     @ApiOperation(value = "회원 단일 조회", notes = "userId로 회원을 조회한다.")
     @GetMapping(value = "/user/{msrl}")
-    public SingleResult<User> findUserById(@ApiParam(value = "회원번호", required = true) @PathVariable long msrl) {
+    public SingleResult<User> findUserById(
+            @ApiParam(value = "회원번호", required = true) @PathVariable long msrl,
+            @ApiParam(value = "언어", defaultValue = "ko") @RequestParam String lang) {
         // 결과 데이터가 단일건인 경우 getBasicResult를 이용해서 결과를 출력한다.
         return responseService.getSingleResult(userJpaRepo.findById(msrl).orElseThrow(CUserNotFoundException::new));
     }
